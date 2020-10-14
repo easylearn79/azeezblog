986f3bedf3e21e4910eddde21af3f823ff3f405b
azeezdon <azeezdonself@gmail.com>
2020-10-14 02:17:48 +0100

    first commit

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import django_heroku
 import dj_database_url 
 import os
 
@@ -187,4 +186,3 @@ TINYMCE_DEFAULT_CONFIG = {
     'statusbar': True,
 }
 
-django_heroku.settings(locals())
