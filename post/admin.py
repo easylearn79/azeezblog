2ff74a6257f05f8716d0fa7545e337fa2acd9c37
azeezdon <azeezdonself@gmail.com>
2020-10-18 09:48:28 +0100

    twaek model

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
-from .models import Author, Post, Comment,Category
+from .models import Author, Post, Comment,Category,Photo
   
 admin.site.register(Author)
+admin.site.register(Photo)
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment, MPTTModelAdmin)
