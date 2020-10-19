e36d70e066a30476ff61cf7b561a99f16d472a5f
azeezdon <azeezdonself@gmail.com>
2020-10-20 00:38:46 +0100

    tweak settings

--- blog/settings.py
+++ blog/settings.py
@@ -16,8 +16,8 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from dotenv import load_dotenv
-load_dotenv()
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY =os.getenv('SECRET_KEY')
+SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
