5399516541623d13264154196c3a280a3b926612
azeezdon <azeezdonself@gmail.com>
2020-10-20 03:06:27 +0100

    tweak meta

--- post/models.py
+++ post/models.py
@@ -70,6 +70,7 @@ class Post(ModelMeta,models.Model):
     hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     image_url = models.CharField(max_length=2089, null=True)
+    image = models.ImageField()
     category = models.ForeignKey(Category, related_name='posts', on_delete=models.PROTECT, default='uncategorized', blank=True)
     content = HTMLField('content')
     previous_post = models.ForeignKey(
@@ -85,13 +86,13 @@ class Post(ModelMeta,models.Model):
 
     _metadata = {
         'title': 'title',
-        'description': 'content',
-        'image_url': 'get_meta_image'
+        'description': 'overview',
+        'image': 'get_meta_image'
     }
 
     def get_meta_image(self):
-        if self.image_url:
-            return self.image_url.url
+        if self.image:
+            return self.image.url
 
     def __str__(self):
         return self.title
